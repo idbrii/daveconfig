c11b94273e0374cce0fa4e62d70406d2e0feb59d
pydave <pydave@gmail.com>
2009-04-23 11:18:20 -0700

    Add missing quotes

--- multi/vim/vimsetup/win_copyvimrc.bat
+++ multi/vim/vimsetup/win_copyvimrc.bat
@@ -3,5 +3,5 @@
 set destination=%USERPROFILE%
 echo Copying vimrc and gvimrc to %destination%
 
-copy .vimrc %destination%\_vimrc
-copy .gvimrc %destination%\_gvimrc
+copy .vimrc  "%destination%\_vimrc"
+copy .gvimrc "%destination%\_gvimrc"
