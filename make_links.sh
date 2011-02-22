781ef9670b7d9a8162f635c4f3afc476b7d39bb3
pydave <pydave@gmail.com>
2011-02-22 12:45:56 -0800

    Fix make_links for new unix file structure
    bash was moved to terminal and there's the new screenrc

--- make_links.sh
+++ make_links.sh
@@ -9,12 +9,24 @@ ln -s $dconfig_path/multi/vim/.gvimrc ~/.
 ln -s $dconfig_path/multi/git/.gitconfig ~/.
 ln -s $dconfig_path/multi/git/.gitignore ~/.
 
-ln -s $dconfig_path/unix/bash/.bash_aliases ~/.
+
+config_dir=$dconfig_path/unix/terminal/
+for a in `ls $config_dir` ;do
+    ln -s $config_dir/$a ~/.$a
+done
+
 # bashrc is different on different platforms
+function delete_if_symbolic() {
+    file=$1
+    if [ -L $file ] ; then
+        rm $file
+    fi
+}
 if [ `uname` = 'Linux' ] ; then
-    ln -s $dconfig_path/unix/bash/.bashrc ~/.
+    # linux uses bashrc, get rid of the profile or it will interfere
+    delete_if_symbolic ~/.profile
 else
-    # if mac
-    ln -s $dconfig_path/unix/bash/.profile ~/.
+    # mac uses profile, get rid of the bashrc
+    delete_if_symbolic ~/.bashrc
 fi
 
