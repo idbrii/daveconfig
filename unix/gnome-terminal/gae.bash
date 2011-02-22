30e6284e90987f231dc07d810d9122ed28ee8190
pydave <pydave@gmail.com>
2011-02-22 12:34:53 -0800

    Add documentation
    Add some comments explaining what the profiles are for and how to set
    them up.

--- unix/gnome-terminal/gae.bash
+++ unix/gnome-terminal/gae.bash
@@ -1,4 +1,6 @@
-# use regular bash setup and virtualenv
+# Use regular bash setup + virtualenv
+# Calling activate directly means no bash customizations (aliases, etc)
+# Custom command: bash --rcfile /full/path/to/this/file
 source /home/$USER/.bashrc
 source /home/$USER/data/code/gae/py25/bin/activate
 
--- unix/gnome-terminal/screen-gnome-terminal.bash
+++ unix/gnome-terminal/screen-gnome-terminal.bash
@@ -1,4 +1,7 @@
 #! /bin/bash -i
+# Each terminal is a screen session. Closing one session (Ctrl-d) connects to
+# the next available session. Use Ctrl-a d to close the window.
+# Custom command: /full/path/to/this/file
 # Source: http://agentzlerich.blogspot.com/2008/07/using-gnu-screen-with-gnome-terminals.html
 
 # First, construct a new window identifier
