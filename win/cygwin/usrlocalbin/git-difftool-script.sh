7d4a545eec4b635d63d0d1f0c7c86f07f893c9b5
pydave <pydave@gmail.com>
2011-09-22 16:16:05 -0700

    Add some cygwin tools
    Some for git, some for perforce, and some for vim.

---
+++ win/cygwin/usrlocalbin/git-difftool-script.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+/c/david/BeyondCompare3/BComp.exe %base %mine /title1=%bname /title2=%yname /leftreadonly
+
+/c/david/BeyondCompare3/BComp.exe %mine %theirs %base %merged /title1=%yname /title2=%tname /title3=%bname /title4=%mname
+
---
+++ win/cygwin/usrlocalbin/git-mergetool-script.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+/c/david/BeyondCompare3/BComp.exe %mine %theirs %base %merged /title1=%yname /title2=%tname /title3=%bname /title4=%mname
+
