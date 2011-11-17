aa17567a1c884c320ff532024a0f88d1ddbd3661
pydave <pydave@gmail.com>
2011-11-17 11:59:18 -0800

    Switch from beyond compare to p4merge
    Beyond compare now requires a pro license for 3-way merges.

--- win/cygwin/usrlocalbin/mergetool.git.sh
+++ win/cygwin/usrlocalbin/mergetool.git.sh
@@ -4,5 +4,9 @@ LOCAL=`cygpath -wa $1`
 REMOTE=`cygpath -wa $2`
 BASE=`cygpath -wa $3`
 MERGED=`cygpath -wa $4`
-/c/david/BeyondCompare3/BComp.exe $LOCAL $THEIRS $BASE $MERGED
 
+# Beyond compare now requires a pro license for 3-way merges.
+#/c/david/BeyondCompare3/BComp.exe $LOCAL $THEIRS $BASE $MERGED
+
+# Info from: http://www.andymcintosh.com/?p=33
+/c/david/Perforce/p4merge.exe "$BASE" "$LOCAL" "$REMOTE" "$MERGED"
