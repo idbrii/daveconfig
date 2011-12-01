fe84de522a9e4f15c4c80eef2770a0ced4bdb3a7
pydave <pydave@gmail.com>
2011-12-01 14:30:35 -0800

    Support multiple tag directories and C#
    Allow you to pass in directories as arguments. Defaults to current.
    
    Add C# support.
    
    Add full path note. I wonder if there's a difference between mlcscope
    and cscope (on linux)?

--- multi/vim/.vim/scripts/build_alttagfiles.sh
+++ multi/vim/.vim/scripts/build_alttagfiles.sh
@@ -1,7 +1,9 @@
-#! /bin/sh
+#! /bin/bash
 # vim:set fdm=marker:
 
 # Build the LookupFile and cscope databases
+# usage:
+#   buildtags.sh cscope lang
 
 # Installing dependents:
 #   aptinstall exuberant-ctags cscope
@@ -15,6 +17,17 @@ fi
 
 filetype=$2
 
+search_dirs=.
+if [ $# -gt 2 ] ; then
+    # search directories passed in
+
+	# ignore cscope and filetype
+	shift
+	shift
+	# use remaining parameters
+    search_dirs=$*
+fi
+
 tagdir=$PWD
 tagfile=$tagdir/filenametags
 
@@ -25,7 +38,11 @@ cd $tagdir
 echo "!_TAG_FILE_SORTED	2	/2=foldcase/"> $tagfile
 if [ "$filetype" == "cpp" ] ; then
     # Probably a big c++ project, so use the simple format
-    find . -type f \( -iname "*.cpp" -o -iname "*.h" \) -printf "%f\t%p\t1\n" | sort -f >> $tagfile
+    find $search_dirs -type f \( -iname "*.cpp" -o -iname "*.h" \) -printf "%f\t%p\t1\n" | sort -f >> $tagfile
+
+elif [ "$filetype" == "cs" ] ; then
+    # C sharp code. don't include examples which are often alongside.
+    find $search_dirs -not \( -name "examples" -prune \) -a -not \( -name "obj" -prune \) -a \( -type f -iname "*.cs" -o -iname "*.xaml" \) -printf "%f\t%p\t1\n" | sort -f >> $tagfile
 
 elif [ "$filetype" == "android" ] ; then
     # Android uses java and xml. Assume we're in the source directory
@@ -33,16 +50,19 @@ elif [ "$filetype" == "android" ] ; then
 
 elif [ "$filetype" == "java" ] ; then
     # The only types we're interested in are java
-    find . -type f -iname "*.java" -printf "%f\t%p\t1\n" | sort -f >> $tagfile
+    find $search_dirs -type f -iname "*.java" -printf "%f\t%p\t1\n" | sort -f >> $tagfile
 
 else
     # Don't know what we are so include anything that's not binary or junk (from vimdoc)
     # DavidAdd: Files: .git tags filenametags cscope.files
     # DavidAdd: Filetypes: pyc out
     # DavidAdd: Folder: v (for virtualenv)
-    find . \( -name .git -o -name v -o -name .svn -o -name .bzr -o -name tags -o -name filenametags -o -name cscope.files -o -wholename ./classes \) -prune -o -not -iregex '.*\.\(pyc\|jar\|gif\|jpg\|class\|exe\|dll\|pdd\|sw[op]\|xls\|doc\|pdf\|zip\|tar\|ico\|ear\|war\|dat\|out\).*' -type f -printf "%f\t%p\t1\n" | sort -f >> $tagfile
+    find $search_dirs \( -name .git -o -name v -o -name .svn -o -name .bzr -o -name tags -o -name filenametags -o -name cscope.files -o -wholename ./classes \) -prune -o -not -iregex '.*\.\(pyc\|jar\|gif\|jpg\|class\|exe\|dll\|pdd\|sw[op]\|xls\|doc\|pdf\|zip\|tar\|ico\|ear\|war\|dat\|out\).*' -type f -printf "%f\t%p\t1\n" | sort -f >> $tagfile
 
 fi
+
+# convert cygwin paths to windows paths
+sed -i -e"s/.cygdrive.c/c:/g" $tagfile
 
 # }}}
 
@@ -52,6 +72,8 @@ fi
 # cscope needs full paths, so replace the relative path with the fully
 # qualified path
 cut -f2 $tagfile | tail --lines=+2 | sed -e"s|^\./|$tagdir/|" > cscope.files
+# does cscope really need full paths? Try without them
+#cut -f2 $tagfile | tail --lines=+2 > cscope.files
 
 if [ "$filetype" == "python" ] ; then
     # Requires the python package pycscope:
