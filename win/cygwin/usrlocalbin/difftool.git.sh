c1a0469813ff48d94e892de0b19b19499246675d
pydave <pydave@gmail.com>
2012-06-26 09:51:19 -0700

    git difftool: change names of diff vars
    I do not understand why LOCAL is not my file, but
    at least now the names mean something

--- win/cygwin/usrlocalbin/difftool.git.sh
+++ win/cygwin/usrlocalbin/difftool.git.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
-LOCAL=`cygpath -wa $1`
-REMOTE=`cygpath -wa $2`
+theirs=`cygpath -wa $1`
+mine=`cygpath -wa $2`
 BASE=`cygpath -wa $3`
 
 # why do these look wrong?
-#echo local=$LOCAL
-#echo REMOTE=$REMOTE
-#echo BASE=$BASE
-/c/david/BeyondCompare3/BComp.exe $BASE $LOCAL /leftreadonly
+#echo theirs=$LOCAL >> /tmp/out
+#echo mine=$REMOTE >> /tmp/out
+#echo BASE=$BASE >> /tmp/out
+/c/david/BeyondCompare3/BComp.exe $theirs $mine /leftreadonly
 
--- win/cygwin/usrlocalbin/mergetool.git.sh
+++ win/cygwin/usrlocalbin/mergetool.git.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
-LOCAL=`cygpath -wa $1`
-REMOTE=`cygpath -wa $2`
+theirs=`cygpath -wa $1`
+mine=`cygpath -wa $2`
 BASE=`cygpath -wa $3`
 MERGED=`cygpath -wa $4`
 
 # Beyond compare now requires a pro license for 3-way merges.
-#/c/david/BeyondCompare3/BComp.exe $LOCAL $THEIRS $BASE $MERGED
+#/c/david/BeyondCompare3/BComp.exe $theirs $THEIRS $BASE $MERGED
 
 # Info from: http://www.andymcintosh.com/?p=33
-/c/david/Perforce/p4merge.exe "$BASE" "$LOCAL" "$REMOTE" "$MERGED"
+/c/david/Perforce/p4merge.exe "$BASE" "$theirs" "$mine" "$MERGED"
