077fb255ac3fc7054c367a8b405b5eb7f2acd787
pydave <pydave@gmail.com>
2014-07-25 10:25:38 -0700

    Make fc always pretty and work in cygwin
    fc doesn't get any windows path translation, so vim expects /tmp to
    be cygwin's unix /tmp. Link them together in the firstsetup.
    
    Remove the redundant remove_junction block in windows_links. It's just a
    copy of the create_junction. Add a comment explaining how to remove.
    
    Use the sh filetype when editing an fc file.
    
    Also use autocmd consistently.

--- win/cygwin/firstsetup/windows_links.cmd
+++ win/cygwin/firstsetup/windows_links.cmd
@@ -6,14 +6,16 @@ setlocal
 
 set fake_c=c:\cygdrive\c
 
+:: For cygdrive paths
 mkdir %fake_c%
 call :create_junction %fake_c%\david c:\david
 call :create_junction %fake_c%\Users c:\Users
 call :create_junction c:\c %fake_c%
 
-REM call :remove_junction %fake_c%\david c:\david
-REM call :remove_junction %fake_c%\Users c:\Users
-REM call :remove_junction c:\c %fake_c%
+:: Setup tmp to make stuff like fc work from bash.
+call :create_junction c:\tmp c:\cygwin\tmp
+
+:: To remove, replace above create_junction with remove_junction (they take the same args).
 REM rmdir %fake_c%
 
 goto:eof
@@ -23,11 +25,14 @@ goto:eof
 :: Subroutines
 
 :create_junction
+:: Make sure the directories leading up to the destination link exist.
+mkdir %1
+rmdir %1
 :: for junction, the second argument is the existing one
 junction %1 %2
 goto:eof
 
 :remove_junction
-:: for junction, the second argument is the existing one
+:: for junction, the argument is the link
 junction -d %1
 goto:eof
