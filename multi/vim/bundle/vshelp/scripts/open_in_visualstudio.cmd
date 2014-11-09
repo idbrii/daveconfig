342bd9d0d661e277e8b3740a594059e06696ca3b
David Briscoe <idbrii@gmail.com>
2014-11-08 18:46:58 -0800

    Extract vshelp into a repo
    There's not much here, but there are often questions about using Visual
    Studio and vim, so I'll pull it out to something I can share.

--- multi/vim/bundle/vshelp/scripts/open_in_visualstudio.cmd
+++
@@ -1,6 +0,0 @@
-:: Open the input file at the input line.
-::
-:: Bug: The line doesn't work
-::
-
-"\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv" /Edit "%1" /Command "Edit.GoTo %2"
