a7c8c7c809b72bb59f897474a3f7c1bc9bf73e34
David Briscoe <idbrii@gmail.com>
2014-12-19 13:45:43 -0800

    Auto move to first conflict in gvim merge tools
    Auto move to merge file and find first conflict from top.
    
    Now they both diff all files in large window with equal width buffers
    and find the first conflict. .cmd version uses Win32-only simalt to
    maximize.

--- win/cygwin/usrlocalbin/mergetool.gvim.git.sh
+++ win/cygwin/usrlocalbin/mergetool.gvim.git.sh
@@ -5,4 +5,6 @@ mine=`cygpath -wa $2`
 BASE=`cygpath -wa $3`
 MERGED=`cygpath -wa $4`
 
-gvim --nofork -d "$theirs" "$MERGED" "$mine"
+# Diff all files in large window with equal width buffers and find the first
+# conflict.
+gvim --nofork +"set lines=999" +"set columns=9999" +"wincmd =" +"wincmd w" +"normal gg]C" -d "$theirs" "$MERGED" "$mine"
--- win/system/bin/vimmerge.cmd
+++ win/system/bin/vimmerge.cmd
@@ -2,8 +2,9 @@
 :: Location: path-to-this-file
 :: Arguments: %2 %r %1
 
-:: Ensure we're fullscreen and diff all input files.
-gvim.exe "+set lines=999" "+set columns=9999" "+simalt ~x" -c "wincmd =" -d %*
+:: Diff all files in large fullscreen window with equal width buffers and find
+:: the first conflict.
+gvim.exe +"set lines=999" +"set columns=9999" +"simalt ~x" +"wincmd =" +"wincmd w" +"normal gg]C" -d %*
 
 REM Could use videinvoke instead?
 REM gvim.exe -S c:/Users/davidb/.vim/videinvoke.vim -c "wincmd =" -d %*
