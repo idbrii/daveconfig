1e4230eddf304168ff596968cd8185fd9fc279e7
David Briscoe <idbrii@gmail.com>
2015-08-12 16:19:11 -0700

    Change vim merge to use git-standard arguments
    Most git tools seem to use the same order:
        BASE LOCAL REMOTE MERGED
    
    Switch vimmerge to be consistent with diffconflicts and p4
    documentation.

--- win/system/bin/vimmerge.cmd
+++ win/system/bin/vimmerge.cmd
@@ -1,10 +1,18 @@
 :: For perforce merge:
 :: Location: path-to-this-file
-:: Arguments: %2 %r %1
+:: Arguments in p4v: %b %2 %1 %r
+::		Base file: %b
+::		Your/Target file: %2
+::		Their/Source file: %1
+::		Result file: %r
+set BASE=%1
+set LOCAL=%2
+set REMOTE=%3
+set MERGED=%4
 
 :: Diff all files in large fullscreen window with equal width buffers and find
 :: the first conflict.
-gvim.exe +"set lines=999" +"set columns=9999" +"simalt ~x" +"wincmd =" +"wincmd w" +"normal gg]C" -d %*
+gvim.exe +"set lines=999" +"set columns=9999" +"simalt ~x" +"wincmd =" +"wincmd w" +"normal gg]C" -d %LOCAL% %MERGED% %REMOTE%
 
 REM Could use videinvoke instead?
 REM gvim.exe -S c:/Users/davidb/.vim/videinvoke.vim -c "wincmd =" -d %*
