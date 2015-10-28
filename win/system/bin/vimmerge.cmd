ebf15e14d603012f07a2ce941877816fd39476dc
David Briscoe <idbrii@gmail.com>
2015-10-28 16:51:45 -0700

    Support weird files in paths for vimmerge
    vimmerge was failing with python merges from p4v. Looks like it didn't
    like (MERGE)[0].py in the if statement. Instead, just assign to a
    variable and check if the variable is empty.
    
    Quote the input arguments. This probably solves the real problem.

--- win/system/bin/vimmerge.cmd
+++ win/system/bin/vimmerge.cmd
@@ -5,25 +5,24 @@
 ::		Your/Target file: %2
 ::		Base file: %b
 ::		Result file: %r
-set LOCAL=%1
-set REMOTE=%2
-set BASE=%3
-set MERGED=%4
+set LOCAL="%1"
+set REMOTE="%2"
+set BASE="%3"
+set MERGED="%4"
 
-set USE_diffconflicts=1
 :: Always use normal vimdiff for diffs.
-if "%MERGED%" == "" (
-	set USE_diffconflicts=0
-)
+set USE_diffconflicts=%MERGED%
+:: Uncomment to disable diffconflicts.
+REM set USE_diffconflicts=
 
-if "%USE_diffconflicts%" == "0" (
-	REM Diff all files in large fullscreen window with equal width buffers and
-	REM find the first conflict.
-	gvim.exe +"set lines=999" +"set columns=9999" +"simalt ~x" +"wincmd =" +"wincmd w" +"normal gg]C" -d %REMOTE% %MERGED% %LOCAL%
-) else (
+if defined USE_diffconflicts (
 	REM diffconflicts works well for merging.
 	set IS_PERFORCE=1
 	%cyg_path%\bash %~dp0\..\..\..\multi\git\tool\mergetool.diffconflicts.git.sh gvim %BASE% %LOCAL% %REMOTE% %MERGED%
+) else (
+	REM Diff all files in large fullscreen window with equal width buffers and
+	REM find the first conflict.
+	gvim.exe +"set lines=999" +"set columns=9999" +"simalt ~x" +"wincmd =" +"wincmd w" +"normal gg]C" -d %REMOTE% %MERGED% %LOCAL%
 )
 
 REM Could use videinvoke instead?
