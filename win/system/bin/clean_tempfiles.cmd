4e846557a190763af25a7139ad5a4ddf9a0726cb
David Briscoe <idbrii@gmail.com>
2017-01-06 11:12:09 -0800

    Add easy way to clean tempfiles
    Unfortunately, these tools don't clean up after themselves and Windows
    doesn't clean up its temp file.
    
    Today I removed 22 GB of csearch files. The other ones are negligible.

---
+++ win/system/bin/clean_tempfiles.cmd
@@ -0,0 +1,8 @@
+:: Today I removed 22 GB of csearch files by doing the below command.
+del %TEMP%\csearch*
+
+:: Vim
+del %TEMP%\VI*.tmp %TEMP%\VI*.tmp.*
+
+:: p4v temp files
+del %TEMP%\t*.tmp*(*)*
