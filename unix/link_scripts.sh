e57d17c7a0bb8873176ce66e3b7c30a074ab620d
David Briscoe <idbrii@gmail.com>
2017-12-21 22:52:58 -0800

    Clean up script linker

--- unix/link_scripts.sh
+++ unix/link_scripts.sh
@@ -1,1 +1,4 @@
-ln -s $PWD/scripts/* ~/data/scripts/.
+# Usually want all scripts in daveconfig.
+ln -s $PWD/scripts ~/data/scripts
+# SOmetimes only want some (most others are junk).
+#~ ln -s --target-directory=~/data/scripts $PWD/scripts
