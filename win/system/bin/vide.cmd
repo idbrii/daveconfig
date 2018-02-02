874975d5ae6c0c032681f9c9d31777936b645a13
David Briscoe <idbrii@gmail.com>
2018-02-02 14:53:52 -0800

    Don't require a file to use vide

--- win/system/bin/vide.cmd
+++ win/system/bin/vide.cmd
@@ -1,2 +1,11 @@
 :: Create a central server to edit in vim
-call gvim.bat --servername VIDE --remote-silent %*
+
+:: Since we use remote-silent, we must send a file!
+set placeholder_file=%1
+if defined placeholder_file (
+        set placeholder_file=
+) else (
+    set placeholder_file=hello-new-server
+)
+
+call gvim.bat --servername VIDE --remote-silent %placeholder_file% %*
