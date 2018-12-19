922b641c3f285bc4aa5ff40e1f8a00a0b414c9f6
David Briscoe <idbrii@gmail.com>
2018-12-19 09:53:14 -0800

    Fix whitespace in vide

--- win/system/bin/vide.cmd
+++ win/system/bin/vide.cmd
@@ -3,7 +3,7 @@
 :: Since we use remote-silent, we must send a file!
 set placeholder_file=%1
 if defined placeholder_file (
-        set placeholder_file=
+    set placeholder_file=
 ) else (
     set placeholder_file=hello-new-server
 )
