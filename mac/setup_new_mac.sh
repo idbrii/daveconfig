02af46cbd53569ffae101f68935b126e0522e168
David Briscoe <idbrii@gmail.com>
2019-02-14 07:55:38 -0800

    Love setup for new mac

--- mac/setup_new_mac.sh
+++ mac/setup_new_mac.sh
@@ -24,6 +24,8 @@ packages="$packages python python3"
 packages="$packages luarocks"
 packages="$packages cscope ctags"
 packages="$packages opensharp-mono"
+# Markdown preview
+packages="$packages pandoc"
 # TODO: Which one to use:
 packages="$packages macvim"
 casks="$casks vimr"
@@ -51,7 +53,27 @@ brew cask install spectacle
 brew unlink vim
 
 # gamedev
-brew cask install unity steam
+brew cask install steam
+
+brew tap wooga/unityversions
+brew cask install unity@version_here
+
+
+# Lua/Love
+luarocks install love
+
+brew install openssl
+luarocks install loverboy CRYPTO_DIR=/usr/local/opt/openssl/ OPENSSL_DIR=/usr/local/opt/openssl
+
+brew install libzip
+brew install p7zip
+#~ luarocks install lua-zip ZIP_DIR=/usr/local
+luarocks install --server=http://luarocks.org/dev lua-lsp ZIP_DIR=/usr/local
+
+luarocks install luacheck
+luarocks install penlight
+# Can't -- version of love is too new for imgui
+#~ luarocks --server=http://luarocks.org/dev --lua-dir=/usr/local/opt/lua@5.1 install love-imgui
 
 
 echo "Get NTFS driver for mac for Seagate drives: https://www.seagate.com/ca/en/support/downloads/item/ntfs-driver-for-mac-os-master-dl/"
