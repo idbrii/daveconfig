afbba95f150b890023545e5f740854726afbf554
David Briscoe <idbrii@gmail.com>
2020-02-28 15:41:12 -0800

    Comment why I like the batch file
    I forgot why when switching gvim to scoop.

--- win/system/bin/vide.cmd
+++ win/system/bin/vide.cmd
@@ -8,4 +8,5 @@ if defined placeholder_file (
     set placeholder_file=hello-new-server
 )
 
+:: Call the .bat because it doesn't leave our command prompt sticking around.
 call gvim.bat --servername VIDE --remote-silent %placeholder_file% %*
