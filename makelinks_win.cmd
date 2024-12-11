9f3d60fb611e9b1d1bb9b358384a9031eecffe42
David Briscoe <idbrii@gmail.com>
2024-12-11 11:54:35 -0800

    nvim: Use forward slashes when creating init.lua
    Convert daveconfig var to use forward slashes for nvim otherwise it
    complains about escape sequences.
    
    Also create the directory if missing.

--- makelinks_win.cmd
+++ makelinks_win.cmd
@@ -21,7 +21,8 @@ mklink    %USERPROFILE%\.screenrc        %daveconfig%\unix\terminal\screenrc
 mklink /D %USERPROFILE%\.vim             %daveconfig%\multi\vim
 echo source %daveconfig%\multi\vim\bundle\aa-david\gvimrc.vim> %USERPROFILE%\.gvimrc
 echo source %daveconfig%\multi\vim\bundle\aa-david\vimrc.vim > %USERPROFILE%\.vimrc
-echo vim.api.nvim_command('source %daveconfig%/multi/vim/bundle/aa-david/vimrc.vim') > %LocalAppData%\nvim\init.lua
+mkdir %LocalAppData%\nvim\
+echo vim.api.nvim_command('source %daveconfig:\=/%/multi/vim/bundle/aa-david/vimrc.vim') > %LocalAppData%\nvim\init.lua
 
 
 mklink    %USERPROFILE%\.gitconfig       %daveconfig%\multi\git\.gitconfig
