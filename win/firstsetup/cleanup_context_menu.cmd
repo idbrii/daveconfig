8bf60a38f13e98d7919275dbb1bec996f3fd1718
David Briscoe <idbrii@gmail.com>
2025-04-16 07:43:15 -0700

    Document how to remove Clipchamp context menu

--- win/firstsetup/cleanup_context_menu.cmd
+++ win/firstsetup/cleanup_context_menu.cmd
@@ -2,6 +2,10 @@
 :: https://winaero.com/remove-edit-in-notepad-context-menu/
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked" /v "{CA6CC9F1-867A-481E-951E-A28C5E4F01EA}" /d "Block Notepad" /t REG_SZ /f
 
+:: Remove Edit in Clipchamp
+:: https://www.elevenforum.com/t/add-or-remove-edit-with-clipchamp-context-menu-in-windows-11.6882/
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked" /v "{8AB635F8-9A67-4698-AB99-784AD929F3B4}" /d "Block Clipchamp" /t REG_SZ /f
+
 
 :: Remove Open in Visual Studio
 :: https://gist.github.com/en0ndev/0e70e5f07296fbfe3dd9628cd19615da
@@ -10,3 +14,4 @@ reg copy "HKEY_CLASSES_ROOT\Directory\shell\AnyCode" "HKEY_CLASSES_ROOT\Director
 
 reg delete "HKEY_CLASSES_ROOT\Directory\Background\shell\AnyCode" /f
 reg delete "HKEY_CLASSES_ROOT\Directory\shell\AnyCode" /f
+
