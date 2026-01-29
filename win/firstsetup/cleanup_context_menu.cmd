ac46ee1fbdddcf82dd367b07ecc9f85511eaf1da
David Briscoe <idbrii@gmail.com>
2026-01-29 08:32:47 -0800

    Add more "Edit in" blockers for Win11
    They keep adding more junk to the context menu instead of making the
    existing "Edit" feature work better.

--- win/firstsetup/cleanup_context_menu.cmd
+++ win/firstsetup/cleanup_context_menu.cmd
@@ -7,6 +7,19 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked" /v "{8AB635F8-9A67-4698-AB99-784AD929F3B4}" /d "Block Clipchamp" /t REG_SZ /f
 
 
+:: Remove Edit in Photos
+:: https://www.elevenforum.com/t/add-or-remove-photos-context-menu-in-windows-10-and-windows-11.28302/
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked"  /t REG_SZ /f /v "{BFE0E2A4-C70C-4AD7-AC3D-10D1ECEBB5B4}" /d "Edit with Photos"
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked"  /t REG_SZ /f /v "{7A53B94A-4E6E-4826-B48E-535020B264E5}" /d "Create with Designer"
+REM reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked"  /t REG_SZ /f /v "{1100CBCD-B822-43F0-84CB-16814C2F6B44}" /d "Erase Object with Photos"
+REM reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked"  /t REG_SZ /f /v "{9AAFEDA2-97B6-43EA-9466-9DE90501B1B6}" /d "Visual Search with Bing"
+
+
+:: Remove Edit in Paint
+:: https://www.elevenforum.com/t/add-or-remove-edit-with-paint-context-menu-in-windows-11.30357/
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked"  /t REG_SZ /f /v "{2430F218-B743-4FD6-97BF-5C76541B4AE9}" /d "Edit with Paint"
+
+
 :: Remove Open in Visual Studio
 :: https://gist.github.com/en0ndev/0e70e5f07296fbfe3dd9628cd19615da
 reg copy "HKEY_CLASSES_ROOT\Directory\Background\shell\AnyCode" "HKEY_CLASSES_ROOT\Directory\Background\shell\AnyCode_Disabled" /s
